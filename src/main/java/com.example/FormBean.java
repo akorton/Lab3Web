8a6562746a4e7f3cee86f707506301287ca66736
akorton <akortysh@yandex.ru>
2022-11-17 20:16:50 +0300

    Add application scope bean, submit button and mechanism and check method

--- src/main/java/com.example/FormBean.java
+++ src/main/java/com.example/FormBean.java
@@ -4,6 +4,7 @@ public class FormBean {
     private double x;
     private double y;
     private double r;
+    private boolean result;
 
     public void setX(double x) {
         this.x = x;
@@ -27,5 +28,26 @@ public class FormBean {
 
     public double getY() {
         return y;
+    }
+
+    private boolean checkSquare(double x, double y, double r){
+        return x >= -r && x <= 0 && y >= -r && y <= 0;
+    }
+
+    private boolean checkQuarterCircle(double x, double y, double r){
+        return x >= 0 && y <= 0 && x*x + y*y <= (r / 2) * (r / 2);
+    }
+
+    private boolean checkTriangle(double x, double y, double r){
+        return x <= 0 && y >= 0 && y - 2 * x <= r;
+    }
+
+    private boolean check(double x, double y, double r){
+        return checkSquare(x, y, r) && checkQuarterCircle(x, y, r) && checkTriangle(x, y, r);
+    }
+
+    public void submit(){
+        this.result = check(x, y, r);
+        System.out.println(x + " " + y + " " + r + " " + result);
     }
 }
---
+++ src/main/java/com.example/TableBean.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import javax.faces.bean.ManagedProperty;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TableBean {
+    private List<FormBean> table = new LinkedList<>();
+}
