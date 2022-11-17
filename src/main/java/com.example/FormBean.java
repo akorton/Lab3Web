9176bc8dc664ca76c3c0e4c73b806a7b065bebf7
akorton <akortysh@yandex.ru>
2022-11-17 17:54:37 +0300

    Styles for main

---
+++ src/main/java/com.example/FormBean.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class FormBean {
+    private double x;
+    private double y;
+    private double r;
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
