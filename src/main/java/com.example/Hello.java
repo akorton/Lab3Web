5c09c863690955f61682d07e417fad35233b9004
akorton <akortysh@yandex.ru>
2022-11-17 15:00:39 +0300

    navigation working. Hello page done

--- src/main/java/com.example/Hello.java
+++ src/main/java/com.example/Hello.java
@@ -1,12 +1,7 @@
 package com.example;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import java.time.LocalDateTime;
 import java.util.Date;
 
-@ManagedBean(name="hello")
-@RequestScoped
 public class Hello {
 
     private Date date;
---
+++ src/main/java/com.example/Navigation.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Navigation {
+
+    public String toMainPage(){
+        return "success";
+    }
+
+    public String toHelloPage(){
+        return "success";
+    }
+}
